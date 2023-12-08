3a811e1c11779d6c948ee9fe25315cd4cda76898
Helena S. Jónsdóttir <h.jonsdottir@columbia.edu>
2023-12-08 15:03:35 -0500

    bug fix

--- make_summaries_alpaca.py
+++ make_summaries_alpaca.py
@@ -3,6 +3,7 @@ import torch
 from transformers import GenerationConfig, LlamaTokenizer, LlamaForCausalLM
 import glob
 import os
+from summary_files import return_summary_files 
 
 def generate_prompt(instruction: str, input_ctxt: str = None) -> str:
     if input_ctxt:
@@ -23,44 +24,44 @@ def generate_prompt(instruction: str, input_ctxt: str = None) -> str:
 
         ### Response:"""
 
-print("Inside file")
 
-tokenizer = LlamaTokenizer.from_pretrained("chainyo/alpaca-lora-7b")
-model = LlamaForCausalLM.from_pretrained(
-    "chainyo/alpaca-lora-7b",
-    load_in_8bit=True,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+# tokenizer = LlamaTokenizer.from_pretrained("chainyo/alpaca-lora-7b")
+# model = LlamaForCausalLM.from_pretrained(
+#     "chainyo/alpaca-lora-7b",
+#     load_in_8bit=True,
+#     torch_dtype=torch.float16,
+#     device_map="auto",
+# )
 
-# early stopping
-# decrease max new tokens
-# stop the beam search
-# Error:
-# CUDA out of memory. Tried to allocate 22.21 GiB. GPU 0 has a total capacty of 14.58 GiB of which 1.08 GiB is free. Including non-PyTorch memory, this process has 13.49 GiB memory in use. Of the allocated memory 9.42 GiB is allocated by PyTorch, and 3.37 GiB is reserved by PyTorch but unallocated. If reserved but unallocated memory is large try setting max_split_size_mb to avoid fragmentation.  See documentation for Memory Management and PYTORCH_CUDA_ALLOC_CONF
-print("Before generation config")
-generation_config = GenerationConfig(
-    # do_sample = True,
-    # temperature=0.2,
-    # top_p=0.75,
-    # top_k=40,
-    # num_beams=2,
-    max_new_tokens=1,
-)
+# # early stopping
+# # decrease max new tokens
+# # stop the beam search
+# # Error:
+# # CUDA out of memory. Tried to allocate 22.21 GiB. GPU 0 has a total capacty of 14.58 GiB of which 1.08 GiB is free. Including non-PyTorch memory, this process has 13.49 GiB memory in use. Of the allocated memory 9.42 GiB is allocated by PyTorch, and 3.37 GiB is reserved by PyTorch but unallocated. If reserved but unallocated memory is large try setting max_split_size_mb to avoid fragmentation.  See documentation for Memory Management and PYTORCH_CUDA_ALLOC_CONF
+# print("Before generation config")
+# generation_config = GenerationConfig(
+#     # do_sample = True,
+#     # temperature=0.2,
+#     # top_p=0.75,
+#     # top_k=40,
+#     # num_beams=2,
+#     max_new_tokens=1,
+# )
 
-model.eval()
-if torch.__version__ >= "2":
-    model = torch.compile(model)
-## END OF CODE SEGMENT FROM HERE: https://huggingface.co/chainyo/alpaca-lora-7b
+# model.eval()
+# if torch.__version__ >= "2":
+#     model = torch.compile(model)
+# ## END OF CODE SEGMENT FROM HERE: https://huggingface.co/chainyo/alpaca-lora-7b
 
 # Specify the folder path and file pattern
-folder_path = 'Outputs/txt_files'
-file_pattern = '*.txt'  # Example: List all .txt files
+# folder_path = 'Outputs/txt_files'
+# file_pattern = '*.txt'  # Example: List all .txt files
 
-# Use glob to get a list of files that match the pattern
-files = glob.glob(os.path.join(folder_path, file_pattern))
-print(files)
-print("before filename in files")
+# # Use glob to get a list of files that match the pattern
+# files = glob.glob(os.path.join(folder_path, file_pattern))
+
+files = return_summary_files()
+
 try:
     for filename in files:
         print("inside filename in files")
@@ -68,7 +69,8 @@ try:
         new_summary_file_name = filename[:-4].replace("/txt_files/", "/summaries_alpaca_lora/")+'.txt'
         new_summary_file_name = new_summary_file_name.replace("Outputs", "AlpacaOutputs")
         print("before if os path")
-        if os.path.exists(summary_file_name) and not os.path.exists(new_summary_file_name):
+        # os.path.exists(summary_file_name) and 
+        if not os.path.exists(new_summary_file_name):
             try: 
                 ### THIS CODE IS TAKEN FROM HERE: https://huggingface.co/chainyo/alpaca-lora-7b
                 instruction = "Summarize the text in the input."
